9dc2a9a49efd3f794fc3e6036ae6b9df7fccfbcf
Douglas Santos <douglas-rhs@hotmail.com>
2017-09-19 12:15:07 -0300

    New folders about es6 and removing useless files

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-// ARROWS FUNCTIONS 
-
 const express = require('express');
+
 const app = express();
+
 const port = 3000;
+
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-})
+  res.sendFile(path.join(`${__dirname}/src/index.html`));
+});
 
 app.listen(port, () => {
   console.log('Servidor rodando na porta 3000');
-})
-
-
+});
